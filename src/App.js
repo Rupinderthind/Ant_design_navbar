b457260358aac3ac05243570bc966827450697e6
Rupinderthind <rupinderpalthind@gmail.com>
2018-08-25 17:03:46 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <Navbar />
+    );
+  }
+}
+
+export default App;
